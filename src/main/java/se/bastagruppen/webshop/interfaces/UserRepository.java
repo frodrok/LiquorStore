dfeaa1dc6a4f6f3065b63d47df3d5e2d5f775652
frodrok <fredrik@c-0456e353.048-70-73746f29.cust.bredbandsbolaget.se>
2015-11-01 14:13:49 +0100

    how does git work?

--- src/main/java/se/bastagruppen/webshop/interfaces/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package se.bastagruppen.webshop.interfaces;
-
-import se.bastagruppen.webshop.model.User;
-
-/**
- * Created by fredrik on 26/10/15.
- */
-public interface UserRepository extends Repository<User> {
-}
